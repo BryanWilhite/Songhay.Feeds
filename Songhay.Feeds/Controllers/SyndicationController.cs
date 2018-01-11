69bd70e0ef1eed6f4872ec2df883a35a0acf2539
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-11 15:29:05 -0800

    :tractor: stubbed out SyndicationController

---
+++ Songhay.Feeds/Controllers/SyndicationController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Songhay.Feeds.Controllers
+{
+    [Route("api/syndication")]
+    public class SyndicationController : Controller
+    {
+        Dictionary<string, string> _feeds;
+    }
+}
