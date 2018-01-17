77210791952419c9a15c9334acf16e5f2ea8e010
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-16 17:37:26 -0800

    :pencil: :fire: added GetFeed [in need of SyndicationFeed]

--- Songhay.Feeds/Controllers/SyndicationController.cs
+++ Songhay.Feeds/Controllers/SyndicationController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Rss;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using System.Xml;
 
 namespace Songhay.Feeds.Controllers
 {
@@ -20,9 +22,41 @@ namespace Songhay.Feeds.Controllers
             if (this._feeds == null) throw new NullReferenceException("The expected configuration section is not here.");
         }
 
+        [Route("feed/{name}")]
+        public async Task<IActionResult> GetFeed(string name)
+        {
+            if (string.IsNullOrEmpty(name)) return this.BadRequest("The expected feed is not here.");
+
+            var feedLocation = this._feeds[name];
+
+            if (string.IsNullOrEmpty(feedLocation)) return this.NotFound($"The expected feed, {name}, is not here.");
+
+            var items = new List<SyndicationItem>();
+
+            using (var xmlReader = XmlReader.Create(feedLocation, new XmlReaderSettings { Async = true }))
+            {
+                var feedReader = new RssFeedReader(xmlReader);
+
+                while (await feedReader.Read())
+                {
+                    switch (feedReader.ElementType)
+                    {
+                        case SyndicationElementType.Item:
+                            ISyndicationItem item = await feedReader.ReadItem();
+                            items.Add(new SyndicationItem(item));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return this.Ok();
+        }
+
         [Route("info/{feed}")]
         [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
-        public IActionResult GetFeed(string feed)
+        public IActionResult GetFeedInfo(string feed)
         {
             if (string.IsNullOrEmpty(feed)) return this.BadRequest("The expected feed is not here.");
 
