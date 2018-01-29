4dbe79203fa9124389937ce10067fe40aab93715
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-28 20:41:54 -0800

    :pencil: :sparkles: formatters added; StringBuilderExtensions stubbed out

---
+++ Songhay.Feeds/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Songhay.Feeds.Models;
+using System.Text;
+
+namespace Songhay.Feeds.Extensions
+{
+    public static class StringBuilderExtensions
+    {
+        public static void BuildAtom(this StringBuilder builder, SyndicationFeed feed)
+        {
+            if (builder == null) return;
+            if (feed == null) return;
+        }
+
+        public static void BuildRss(this StringBuilder builder, SyndicationFeed feed)
+        {
+            if (builder == null) return;
+            if (feed == null) return;
+        }
+    }
+}
---
+++ Songhay.Feeds/OutputFormatters/AtomOutputFormatter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Songhay.Feeds.Extensions;
+using Songhay.Feeds.Models;
+using Songhay.Models;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Songhay.Feeds.OutputFormatters
+{
+    public class AtomOutputFormatter : TextOutputFormatter
+    {
+        public AtomOutputFormatter()
+        {
+            this.SupportedMediaTypes.Add(MimeTypes.ApplicationAtomXml);
+            this.SupportedMediaTypes.Add(MimeTypes.ApplicationRssXml);
+
+            this.SupportedEncodings.Add(Encoding.UTF8);
+            this.SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var builder = new StringBuilder();
+            var feed = context.Object as SyndicationFeed;
+            builder.BuildAtom(feed);
+            return context.HttpContext.Response.WriteAsync(builder.ToString());
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            if (typeof(SyndicationFeed).IsAssignableFrom(type)) return base.CanWriteType(type);
+
+            return false;
+        }
+    }
+}
