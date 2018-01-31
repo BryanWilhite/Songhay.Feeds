8e8a06816708bda820762e16c36991010ceb4ecb
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-30 18:07:49 -0800

    :hammer: SyndicationFeed props

--- Songhay.Feeds/Models/SyndicationFeed.cs
+++ Songhay.Feeds/Models/SyndicationFeed.cs
@@ -10,30 +10,41 @@ namespace Songhay.Feeds.Models
     /// </summary>
     public class SyndicationFeed
     {
-        /// <summary>Initializes a new instance of the <see cref="T:System.ServiceModel.Syndication.SyndicationFeed" /> class with the specified collection of <see cref="T:System.ServiceModel.Syndication.SyndicationItem" /> objects.</summary>
-        /// <param name="items">A collection of <see cref="T:System.ServiceModel.Syndication.SyndicationItem" /> objects.</param>
-        public SyndicationFeed(IEnumerable<SyndicationItem> items) : this(null, null, null, items)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SyndicationFeed"/> class.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        public SyndicationFeed(IEnumerable<ISyndicationItem> items) : this(null, null, null, items)
         {
         }
 
-        /// <summary>Initializes a new instance of the <see cref="T:System.ServiceModel.Syndication.SyndicationFeed" /> class with the specified title, description, URI, and collection of <see cref="T:System.ServiceModel.Syndication.SyndicationItem" /> objects.</summary>
-        /// <param name="title">The title of the feed.</param>
-        /// <param name="description">The description of the feed.</param>
-        /// <param name="feedAlternateLink">The URI for the feed.</param>
-        /// <param name="items">A collection of <see cref="T:System.ServiceModel.Syndication.SyndicationItem" /> objects.</param>
-        public SyndicationFeed(string title, string description, Uri feedAlternateLink, IEnumerable<SyndicationItem> items) : this(title, description, feedAlternateLink, null, DateTimeOffset.MinValue, items)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SyndicationFeed"/> class.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="feedAlternateLink">The feed alternate link.</param>
+        /// <param name="items">The items.</param>
+        public SyndicationFeed(string title, string description, Uri feedAlternateLink, IEnumerable<ISyndicationItem> items) : this(title, description, feedAlternateLink, null, DateTimeOffset.MinValue, items)
         {
         }
 
-        /// <summary>Creates a new instance of the <see cref="T:System.ServiceModel.Syndication.SyndicationFeed" /> class. </summary>
-        /// <param name="title">The syndication feed title.</param>
-        /// <param name="description">The syndication feed description.</param>
-        /// <param name="feedAlternateLink">The alternate URI for the syndication feed.</param>
-        /// <param name="id">The ID of the syndication feed.</param>
-        /// <param name="lastUpdatedTime">The <see cref="T:System.DateTimeOffset" /> that contains the last time the syndication feed was updated.</param>
-        /// <param name="items">A collection of <see cref="T:System.ServiceModel.Syndication.SyndicationItem" /> objects.</param>
-        public SyndicationFeed(string title, string description, Uri feedAlternateLink, string id, DateTimeOffset lastUpdatedTime, IEnumerable<SyndicationItem> items)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SyndicationFeed"/> class.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="feedAlternateLink">The feed alternate link.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="lastUpdatedTime">The last updated time.</param>
+        /// <param name="items">The items.</param>
+        public SyndicationFeed(string title, string description, Uri feedAlternateLink, string id, DateTimeOffset lastUpdatedTime, IEnumerable<ISyndicationItem> items)
         {
+            this._authors = new Collection<ISyndicationPerson>();
+            this._links = new Collection<ISyndicationLink>();
+
+            this.Categories = new Collection<ISyndicationCategory>();
+
             if (title != null)
             {
                 this.Title = new SyndicationContent(title);
@@ -53,12 +64,38 @@ namespace Songhay.Feeds.Models
         }
 
         /// <summary>
+        /// Gets the authors.
+        /// </summary>
+        /// <value>
+        /// The authors.
+        /// </value>
+        public Collection<ISyndicationPerson> Authors
+        {
+            get
+            {
+                if (this._authors == null)
+                {
+                    this._authors = new NullNotAllowedCollection<ISyndicationPerson>();
+                }
+                return this._authors;
+            }
+        }
+
+        /// <summary>
+        /// Gets the categories.
+        /// </summary>
+        /// <value>
+        /// The categories.
+        /// </value>
+        public Collection<ISyndicationCategory> Categories { get; private set; }
+
+        /// <summary>
         /// Gets or sets the description.
         /// </summary>
         /// <value>
         /// The description.
         /// </value>
-        public SyndicationContent Description { get => _description; set => _description = value; }
+        public ISyndicationContent Description { get => _description; set => _description = value; }
 
         /// <summary>
         /// Gets or sets the identifier.
@@ -74,7 +111,7 @@ namespace Songhay.Feeds.Models
         /// <value>
         /// The items.
         /// </value>
-        public IEnumerable<SyndicationItem> Items { get => _items; set => _items = value; }
+        public IEnumerable<ISyndicationItem> Items { get => _items; set => _items = value; }
 
         /// <summary>
         /// Gets or sets the last updated time.
@@ -84,15 +121,19 @@ namespace Songhay.Feeds.Models
         /// </value>
         public DateTimeOffset LastUpdatedTime { get => lastUpdatedTime; set => lastUpdatedTime = value; }
 
-        /// <summary>Gets the links associated with the feed.</summary>
-        /// <returns>A collection of <see cref="T:System.ServiceModel.Syndication.SyndicationLink" /> objects.</returns>
-        public Collection<SyndicationLink> Links
+        /// <summary>
+        /// Gets the links.
+        /// </summary>
+        /// <value>
+        /// The links.
+        /// </value>
+        public Collection<ISyndicationLink> Links
         {
             get
             {
                 if (this._links == null)
                 {
-                    this._links = new NullNotAllowedCollection<SyndicationLink>();
+                    this._links = new NullNotAllowedCollection<ISyndicationLink>();
                 }
                 return this._links;
             }
@@ -104,15 +145,16 @@ namespace Songhay.Feeds.Models
         /// <value>
         /// The title.
         /// </value>
-        public SyndicationContent Title { get => _title; set => _title = value; }
+        public ISyndicationContent Title { get => _title; set => _title = value; }
 
-        SyndicationContent _description;
+        ISyndicationContent _description;
 
-        IEnumerable<SyndicationItem> _items;
+        IEnumerable<ISyndicationItem> _items;
 
-        Collection<SyndicationLink> _links;
+        Collection<ISyndicationPerson> _authors;
+        Collection<ISyndicationLink> _links;
 
-        SyndicationContent _title;
+        ISyndicationContent _title;
 
         string id;
 
