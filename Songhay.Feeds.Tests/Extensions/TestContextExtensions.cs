e9f1f0d4737296123cc00438b58d3992a2ad77a1
Bryan Wilhite <rasx@songhaysystem.com>
2018-02-03 20:54:17 -0800

    :umbrella: :tractor: ShouldLoadConfiguration() passes

---
+++ Songhay.Feeds.Tests/Extensions/TestContextExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songhay.Extensions;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Songhay.Feeds.Tests.Extensions
+{
+    public static class TestContextExtensions
+    {
+        public static DirectoryInfo ShouldGetShellProjectDirectoryInfo(this TestContext context, Type type)
+        {
+
+            var projectDirectoryInfo = context.ShouldGetTestProjectDirectoryInfo(type);
+            var shellProjectDirectoryInfo = projectDirectoryInfo.Parent.GetDirectories().Single(i => i.Name.EndsWith("Shell"));
+            Assert.IsNotNull(shellProjectDirectoryInfo, "The expected Shell project directory is not here.");
+
+            return shellProjectDirectoryInfo;
+        }
+
+        public static DirectoryInfo ShouldGetTestProjectDirectoryInfo(this TestContext context, Type type)
+        {
+            var projectDirectoryInfo = context
+                            .ShouldGetAssemblyDirectoryInfo(type) // netcoreapp2.0
+                            ?.Parent // Debug or Release
+                            ?.Parent // bin
+                            ?.Parent;
+            context.ShouldFindDirectory(projectDirectoryInfo?.FullName);
+
+            return projectDirectoryInfo;
+        }
+    }
+}
---
+++ Songhay.Feeds.Tests/ProgramTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songhay.Feeds.Tests.Extensions;
+
+namespace Songhay.Feeds.Tests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void ShouldLoadConfiguration()
+        {
+            var shellProjectDirectoryInfo = this.TestContext.ShouldGetShellProjectDirectoryInfo(this.GetType());
+            var configuration = Shell.Program.LoadConfiguration(shellProjectDirectoryInfo.FullName);
+            Assert.IsNotNull(configuration);
+        }
+
+        [TestMethod]
+        [TestProperty("feedsDirectory", "feeds")]
+        public void ShouldLoadFeedsMetadata()
+        {
+            //meta.Feeds.ForEachInEnumerable(feed =>
+            //{
+            //    var uri = new Uri(feed.Value, UriKind.Absolute);
+            //    this.TestContext.WriteLine($"uri: {uri.OriginalString}");
+            //    var client = new HttpClient();
+            //    var request = new HttpRequestMessage() { RequestUri = uri, Method = HttpMethod.Get };
+
+            //    var response = client.SendAsync(request).Result;
+            //    Assert.IsNotNull(response, "The expected response is not here.");
+            //    Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "The expected response status code is not here.");
+
+            //    var content = response.Content.ReadAsStringAsync().Result;
+            //    File.WriteAllText(Path.Combine(feedsDirectory, $"{feed.Key}.xml"), content);
+            //});
+        }
+    }
+}
