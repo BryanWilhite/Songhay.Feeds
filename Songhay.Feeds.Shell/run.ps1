2655bb7cf7b7b391df5a1d014756001a96097fae
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-11-09 12:16:04 -0800

    :pencil2: issue #8

--- Songhay.Feeds.Shell/run.ps1
+++ Songhay.Feeds.Shell/run.ps1
@@ -1,4 +1,10 @@
-﻿Set-Location $PSScriptRoot
+﻿trap 
+{ 
+    Write-Output $_ 
+    exit 1 
+}
+
+Set-Location $PSScriptRoot
 
 & dotnet Songhay.Feeds.Shell.dll DownloadFeedsActivity
 & dotnet Songhay.Feeds.Shell.dll StoreFeedsActivity
