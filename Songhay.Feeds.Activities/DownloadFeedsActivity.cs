ccc096b0f17eca783f11c81dd2b5428e51ea5fb5
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-05-21 12:17:29 -0700

    :hammer: :umbrella: resolves #9

--- Songhay.Feeds.Activities/DownloadFeedsActivity.cs
+++ Songhay.Feeds.Activities/DownloadFeedsActivity.cs
@@ -6,9 +6,9 @@ using Songhay.Feeds.Activities.Extensions;
 using Songhay.Feeds.Models;
 using Songhay.Models;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -57,37 +57,34 @@ Use command-line argument {ProgramArgs.BasePath} to prepend a base path to a con
             });
         }
 
+        internal async Task DownloadFeedAsync(KeyValuePair<string, string> feed, string rootDirectory)
+        {
+            var uri = new Uri(feed.Value, UriKind.Absolute);
+            traceSource?.TraceVerbose($"uri: {uri.OriginalString}");
+            var request = new HttpRequestMessage() { RequestUri = uri, Method = HttpMethod.Get };
+            var response = await request.SendAsync(requestMessageAction: null);
+            if (response == null)
+                throw new NullReferenceException($"The expected response is not here. [{uri.OriginalString}]");
+
+            if (response.StatusCode == HttpStatusCode.Forbidden)
+            {
+                traceSource?.TraceWarning($"WARNING: The request was forbidden. [{uri.OriginalString}]");
+                return;
+            }
+            else if (response.StatusCode != HttpStatusCode.OK)
+                throw new HttpRequestException($"The expected response status code is not here. [{nameof(response.StatusCode)}: {response.StatusCode}] [{uri.OriginalString}]");
+
+            var xml = await response.Content.ReadAsStringAsync();
+            File.WriteAllText(Path.Combine(rootDirectory, $"{feed.Key}.xml"), xml);
+        }
+
         internal void DownloadFeeds(FeedsMetadata meta, string rootDirectory)
         {
-            if (string.IsNullOrEmpty(meta.FeedsDirectory)) throw new NullReferenceException("The expected feeds directory is not configured.");
+            if (string.IsNullOrEmpty(meta.FeedsDirectory))
+                throw new NullReferenceException("The expected feeds directory is not configured.");
 
-            var tasks = meta.Feeds.Select(feed =>
-            {
-                return Task.Run(async () =>
-                {
-                    var uri = new Uri(feed.Value, UriKind.Absolute);
-                    traceSource?.TraceVerbose($"uri: {uri.OriginalString}");
-                    var client = new HttpClient();
-                    var request = new HttpRequestMessage() { RequestUri = uri, Method = HttpMethod.Get };
-
-                    var response = await client.SendAsync(request);
-                    if (response == null)
-                        throw new NullReferenceException($"The expected response is not here. [{uri.OriginalString}]");
-
-                    if (response.StatusCode == HttpStatusCode.Forbidden)
-                    {
-                        traceSource?.TraceWarning($"WARNING: The request was forbidden. [{uri.OriginalString}]");
-                        return;
-                    }
-                    else if (response.StatusCode != HttpStatusCode.OK)
-                        throw new HttpRequestException($"The expected response status code is not here. [{nameof(response.StatusCode)}: {response.StatusCode}] [{uri.OriginalString}]");
-
-                    var xml = await response.Content.ReadAsStringAsync();
-                    File.WriteAllText(Path.Combine(rootDirectory, $"{feed.Key}.xml"), xml);
-                });
-            }).ToArray();
-
-            Task.WaitAll(tasks);
+            foreach (var feed in meta.Feeds)
+                this.DownloadFeedAsync(feed, rootDirectory).GetAwaiter().GetResult();
         }
     }
 }
