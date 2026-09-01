05d85c0438b5332cb10a6271f475b058b839044c
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-08-31 22:02:13 -0700

    run `aspire add docker` ⚙❄✨change `Songhay.Feeds.AppHost/AppHost.cs` to add `.WithExternalHttpEndpoints()` for the `"apiservice"` project 🔨 run `aspire deploy` 🏗🚀

--- Songhay.Feeds.AppHost/AppHost.cs
+++ Songhay.Feeds.AppHost/AppHost.cs
@@ -1,6 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddDockerComposeEnvironment("env");
+
 var apiService = builder.AddProject<Projects.Songhay_Feeds_ApiService>("apiservice")
+    .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
